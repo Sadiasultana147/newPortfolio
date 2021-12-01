3780e48d3fc2c5643d6d19c64d94d2bf88674073
Sadiasultana147 <sadiafency.dev@outlook.com>
2021-12-01 11:24:04 +0600

    8

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -6,59 +6,56 @@ const Project = (props) => {
     const { id, liveLink, gitLink, ProjectName, image1, image2, image3, image4, image5 } = props.project
     return (
         <div >
-            <div >
-                <div className="col h-100">
-                    <div className="cardbox w-100 card h-100">
-
-
-                        <div className="card-body text-center">
-                            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <img className=" d-block w-100 h-100 d-flex justify-content-center" src={image1} alt="..." />
-
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src={image2} class="d-block w-100 h-100" alt="..." />
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src={image3} class="d-block w-100 h-100" alt="..." />
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src={image4} class="d-block w-100 h-100" alt="..." />
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src={image5} class="d-block w-100 h-100" alt="..." />
-                                    </div>
+            <div class="col ">
+                <div class="card  h-100">
+                    <div>
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src={image1} class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={image2} class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={image3} class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={image4} class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src={image5} class="d-block w-100" alt="..." />
                                 </div>
                             </div>
-                            <h4 className="card-text">{ProjectName}</h4>
-                            <hr style={{ border: "2px solid blue" }} />
-
-                            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 pt-5">
-                                <div>
-                                    <a href={liveLink} target="_blank"><button className="btn-grad ">
-                                        Preview
-                                    </button>
-                                    </a>
-                                </div>
-                                <div>
-                                    <Link className="link" to={`details/${id}`}><button className="btn-grad ">
-                                        Details
-                                    </button>
-                                    </Link>
-                                </div>
-                                <div>
-                                    <a href={gitLink} target="_blank"><button className="btn-grad ">
-                                        GITLINK
-                                    </button>
-                                    </a>
-                                </div>
-                            </div>
-
-
                         </div>
+                    </div>
+                    <div class="card-body text-center">
 
+
+
+                        <h4 className="card-text">{ProjectName}</h4>
+                        <hr style={{ border: "2px solid blue" }} />
+
+                        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 pt-5">
+                            <div>
+                                <a href={liveLink} target="_blank"><button className="btn-grad ">
+                                    Preview
+                                </button>
+                                </a>
+                            </div>
+                            <div>
+                                <Link className="link" to={`details/${id}`}><button className="btn-grad ">
+                                    Details
+                                </button>
+                                </Link>
+                            </div>
+                            <div>
+                                <a href={gitLink} target="_blank"><button className="btn-grad ">
+                                    GITLINK
+                                </button>
+                                </a>
+                            </div>
+                        </div>
 
                     </div>
 
